5278e59e0fbca492f6ba8a736c983ac64223cd04
Win <winsukerabbit@gmail.com>
2022-07-24 19:17:18 +0700

    Add args

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 export SSHPASS=@chewathai26
 commandStdout=$(sshpass -e ssh -o StrictHostKeyChecking=no -p 22 rbkadmin@185.78.164.161 "/home/shared/wsg_backend/check_service_start_up_message.sh bns")
-
+echo $1
 echo "::set-output name=command_execution_stdout::$commandStdout"
 
 exit 0
