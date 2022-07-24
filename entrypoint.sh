ab31ee34e26f6dd867265069ebc4100988752118
Win <winsukerabbit@gmail.com>
2022-07-24 19:41:13 +0700

    Update script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-export SSHPASS=@chewathai26
-commandStdout=$(sshpass -e ssh -o StrictHostKeyChecking=no -p 22 rbkadmin@185.78.164.161 "/home/shared/wsg_backend/check_service_start_up_message.sh bns")
-echo $1
+export SSHPASS=$PASSWORD
+
+commandStdout=$(sshpass -e ssh -o StrictHostKeyChecking=no -p $PORT $USERNAME@$HOST "$COMMAND")
+
+echo "::debug::COMMAND is $COMMAND"
+
 echo "::set-output name=command_execution_stdout::$commandStdout"
 
 exit 0
