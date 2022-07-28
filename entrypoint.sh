d0caba6926ab4be78f1368591052b665402bbc84
Win <winsukerabbit@gmail.com>
2022-07-28 07:35:39 +0700

    .'

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-echo $PRIVATE_KEY > ~/.ssh/id_rsa 
-echo $KNOWN_HOSTS > ~/.ssh/known_hosts
 
-commandStdout=$(ssh-p $PORT $USERNAME@$HOST "$COMMAND")
+mkdir "/root/.ssh"
+
+echo $SSH_PRIVATE_KEY > /root/.ssh/id_rsa
+chmod 400 /root/.ssh/id_rsa
+
+echo "Host *" > "/root/.ssh/config"
+echo "  AddKeysToAgent yes" >> "/root/.ssh/config"
+echo "  IdentityFile /root/.ssh/id_rsa" >> "/root/.ssh/config"
+
+commandStdout=$(ssh -v -o StrictHostKeyChecking=no -p $PORT $USERNAME@$HOST "$COMMAND")
 
 echo "::set-output name=command_execution_stdout::$commandStdout"
 
