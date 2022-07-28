39822cf4eae10c037ec767b59f2a5c82ac81ec6b
Win <winsukerabbit@gmail.com>
2022-07-28 08:12:58 +0700

    Correct command

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ echo $SSH_PRIVATE_KEY > /root/.ssh/id_rsa
 chmod 400 /root/.ssh/id_rsa
 
 echo "Host *" > "/root/.ssh/config"
-echo "  AddKeysToAgent yes" >> "/root/.ssh/config"
-echo "  IdentityFile /root/.ssh/id_rsa" >> "/root/.ssh/config"
+echo "AddKeysToAgent yes" >> "/root/.ssh/config"
+echo "IdentityFile /root/.ssh/id_rsa" >> "/root/.ssh/config"
 
-commandStdout=$(ssh -v -o StrictHostKeyChecking=no -p $PORT $USERNAME@$HOST "$COMMAND")
+commandStdout=$(sshpass ssh -v -o StrictHostKeyChecking=no -p $PORT $USERNAME@$HOST "$COMMAND")
 
 echo "::set-output name=command_execution_stdout::$commandStdout"
 
