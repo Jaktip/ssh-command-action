febf35cd9e9f92548755091ff8dcfff78d385514
Win <winsukerabbit@gmail.com>
2022-07-28 18:34:12 +0700

    Remove unnecessary components

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo $PRIVATE_KEY > ~/.ssh/id_rsa
-echo $KNOWN_HOSTS > ~/.ssh/known_hosts
 
 mkdir "/root/.ssh"
 
@@ -9,8 +8,8 @@ echo "$SSH_PRIVATE_KEY" > "/root/.ssh/id_rsa"
 chmod 400 "/root/.ssh/id_rsa"
 
 echo "Host *" > "/root/.ssh/config"
-echo "  AddKeysToAgent yes" >> "/root/.ssh/config"
-echo "  IdentityFile /root/.ssh/id_rsa" >> "/root/.ssh/config"
+echo "AddKeysToAgent yes" >> "/root/.ssh/config"
+echo "IdentityFile /root/.ssh/id_rsa" >> "/root/.ssh/config"
 
 commandStdout=$(sshpass ssh -v -o StrictHostKeyChecking=no -p $PORT $USERNAME@$HOST "$COMMAND")
 
